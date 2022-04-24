096929f1947b347bdc9e06b6096bbc63ccecd4a2
Enrique Vee <40583307+enrique89ve@users.noreply.github.com>
2022-04-23 20:14:34 -0400

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,42 @@
+$("#send_custom").click(function () {
+  console.log("Claim");
+  let keychain = window.hive_keychain
 
-// Send Handshake event
-$("#sw-handshake").click(function () {
-  hive_keychain.requestHandshake(function () {
-    console.log("Handshake received!");
-  });
+
+  let custom = '{"claim":true}'
+  keychain.requestCustomJson(
+  null,
+  "duat_drop_claim",
+  "Posting",
+  custom,
+  null,
+  function (response) {
+
+    console.log("main js response - custom JSON");
+    console.log(response);
+
+
+    if (response.success == true) {
+
+      alert("Successfully!")
+    }
+
+
+
+
+    if (response.error == "user_cancel") {
+
+      alert(response.error)
+    }
+
+
+
+    },
+    null
+
+
+
+
+
+  );
 });
-  
-
-  $("#send_custom").click(function () {
-    console.log("Claim");
-    hive_keychain.requestCustomJson(
-      $("#custom_username").val(),
-      $("#custom_id").val(),
-      $("#custom_method").val(),
-      $("#custom_json").val(),
-      $("#custom_message").val(),
-      function (response) {
-        console.log("main js response - custom JSON");
-        alert(response['message']);
-        console.log(response['message']);
-        
-      },
-      $("#custom_rpc").val()
-
-      
-
-
-
-
-      
-    );
-  });
-
-
